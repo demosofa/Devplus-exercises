4bad3bd8ed7683754ebc2a690965dcb51a10ffb2
demosofa <thientndgcd18746@fpt.edu.vn>
2023-07-18 16:14:35 +0700

    chore: adđ sort

--- atm.js
+++ atm.js
@@ -10,33 +10,47 @@ const K50_VALUE = 50000;
 
 const BASE_UNIT = 50000;
 
-function calculate(amount, lstCheck, result) {
+function calculate(amount, lstCheck, result, valid = false) {
   let { value, left } = lstCheck[0];
   let loop = Math.floor(amount / value);
   if (loop > 0 && loop <= left) {
     amount -= value * loop;
     result["k" + value / 1000] = loop;
   }
+  // if (amount % value == 0) valid = true;
   if (amount == 0) return result;
   else if (lstCheck.length - 1)
-    return calculate(amount, lstCheck.slice(1), result);
-  else if (amount % value) return "Invalid balance";
-  else return "Insufficient balance from ATM";
+    return calculate(amount, lstCheck.slice(1), result, valid);
+  // else if (valid) return "Invalid balance";
+  else return null;
 }
 
-function withdraw(amount, lstCheck) {
+function withdraw(amount, lstCheck, baseUnit) {
+  if (amount % baseUnit != 0) return "Invalid balance";
+  lstCheck = lstCheck.sort((a, b) => b.value - a.value);
   let init = lstCheck.reduce((prev, curr) => {
     prev["k" + curr.value / 1000] = 0;
     return prev;
   }, {});
-  return calculate(amount, lstCheck.slice(0), init);
+  let result;
+  for (let i = 0; i < lstCheck.length; i++) {
+    result = calculate(amount, lstCheck.slice(i), init);
+    // if (result && typeof result == "object") return result;
+    if (result) return result;
+  }
+  // if (result) return result;
+  return "Insufficient balance from ATM";
 }
 
 console.log(
-  withdraw(900000, [
-    { value: K500_VALUE, left: K500_QUANTITY },
-    { value: K200_VALUE, left: K200_QUANTITY },
-    { value: K100_VALUE, left: K100_QUANTITY },
-    { value: K50_VALUE, left: K50_QUANTITY },
-  ])
+  withdraw(
+    65000,
+    [
+      { value: K500_VALUE, left: K500_QUANTITY },
+      { value: K200_VALUE, left: K200_QUANTITY },
+      { value: K100_VALUE, left: K100_QUANTITY },
+      { value: K50_VALUE, left: K50_QUANTITY },
+    ],
+    BASE_UNIT
+  )
 );
